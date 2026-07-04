efb2b5c837010664c139f13991959c0d27b189be
EshikaKumari0204 <eshikakumaribgs@gmail.com>
2026-07-04 13:13:55 +0530

    last frontend deploy

--- client/src/pages/PrintPayslips.jsx
+++ client/src/pages/PrintPayslips.jsx
@@ -7,6 +7,7 @@ import { format } from "date-fns";
 import { PrinterIcon, BuildingIcon } from "lucide-react";
 import api from "../api/axios";
 
+
 const PrintPayslips = () => {
   const [payslip, setPayslip] = useState(null);
   const [loading, setLoading] = useState(true);
